75e3e5dcbd7b955417d7f106bde7ace8c7d183f2
mse56 <mse56@cornell.edu>
2015-10-11 14:23:50 -0400

    Update primary.js

--- js/primary.js
+++ js/primary.js
@@ -60,7 +60,7 @@
 		
 		//Set text height function
 		$(window).scroll(function() {
-			$(''.include').each(function() {
+			$(.include').each(function() {
 				textOffsetBottom = $(window).height() - (parseInt($(this).css('top').replace('px' , '')) + $(this).height());
 				divOffsetBottom = $(window).height()-($(this).closest("section").height()-(-1*($(this).closest("section").offset().top - $(window).scrollTop())));
 				newHeight = Math.max($(this).height() - (divOffsetBottom - textOffsetBottom), 0);
